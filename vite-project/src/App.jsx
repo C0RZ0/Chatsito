0b033c12a50351cb6c80e955033f882c279aa004
Matt <matthew.corzo@estudiantesunibague.edu.co>
2026-05-11 08:58:40 -0500

    Completar App.jsx

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,8 +1,18 @@
+import {useState} from "react"
+
 function App() {
+const [inputMEssage, setInputMessage] = useState("")
+
+
+const handleSubmit = (e) => {
+  e.preventDefault()
+  //Como se envian los mensajes al servidor
+}
+
   return (
     <div>
-      <form>
-        <input type="text" placeholder="Escribe tu mensaje..." />
+      <form onSubmit={handleSubmit}>
+        <input onChange={(e) => setInputMessage(e.target.value)}/>
         <button type="submit">Enviar</button>
       </form>
     </div>
